187afce3445316a3e4a31b095bb6f23e9f8f4022
Whzree <1271596860@qq.com>
2025-04-24 12:41:04 +0800

    Site updated: 2025-04-24 12:41:04

--- js/cursor.js
+++ js/cursor.js
@@ -1,3 +1,31 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
+
+
+
 var CURSOR;
 
 Math.lerp = (a, b, n) => (1 - n) * a + n * b;
